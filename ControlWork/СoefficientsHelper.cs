ffb0d2b63229f2a6db4482350f211deb45f5d45d
Eugene Osipov <85435988+zarasfara@users.noreply.github.com>
2023-11-03 20:46:24 +0500

    Refactor (#14)
    * feat: Коэффициент корреляции
    
    * refactor(calc form): рефактор

--- ControlWork/СoefficientsHelper.cs
+++ ControlWork/СoefficientsHelper.cs
@@ -12,5 +12,19 @@ namespace ControlWork
         {
             return (a1 * averageX) / averageY;
         }
+
+        public static double CoefficientCorelation(long n, int[] array_y, long sum_x, double sum_y, long sum_x_2, long sum_xy)
+        {
+            double sum_y_2 = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum_y_2 += Math.Pow(array_y[i], 2);
+            }
+
+            double coeff_corel = (n * sum_xy - sum_x * sum_y) /
+                Math.Sqrt((n * sum_x_2 - sum_x * sum_x) * (n * sum_y_2 - sum_y * sum_y));
+
+            return coeff_corel;
+        }
     }
 }
